fb1acb55a1401cb49f05be8755f80af1f3b6eafd
Rodrigo Gonzalez <rodrigo@axented.com>
2017-11-08 10:39:51 -0600

    Add interceptor to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-
+import { HttpClientModule, HTTP_INTERCEPTORS  } from '@angular/common/http';
+import { MyHttpInterceptor } from './my-http-interceptor';
 
 import { AppComponent } from './app.component';
 
